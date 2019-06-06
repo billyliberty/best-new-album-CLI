b9456e9fd775772d9c047f0906df229dec513bcc
Joshua Crum <joshua.crum@gmail.com>
2019-06-06 19:26:44 +0000

    Added basics for CLI class.

---
+++ lib/best-new-album/CLI.rb
@@ -0,0 +1,18 @@
+class CLI 
+  
+  def call 
+    album_list
+    menu
+    goodbye
+  end
+  
+  def album_list
+    puts "Best new albums as reviewed by Pitchfork."
+    
+  end
+  
+  def menu
+ 
+  end
+  
+end
