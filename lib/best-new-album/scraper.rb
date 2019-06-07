acdf598ba48246a3003d087de0bc727960eeceb3
Joshua Crum <joshua.crum@gmail.com>
2019-06-07 15:08:34 +0000

    Added to scraper class.

--- lib/best-new-album/scraper.rb
+++ lib/best-new-album/scraper.rb
@@ -1,2 +1,15 @@
 class NewAlbum::Scraper
+  
+  def get_page
+    index_page = Nokogiri::HTML(open("https://pitchfork.com/reviews/best/albums/"))
+  end
+  
+  def scrape_albums
+    self.get_page.css
+  end
+  
+  def make_albums
+  
+  end
+  
 end
